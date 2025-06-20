d718aabdf68d016c239b00c2a7f8507882602a72
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-06-20 22:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+const color = document.getElementById('color-id');
+const container = document.getElementById('container');
+let colorInput = "#000000";
+handleSubmit = () =>{
+	colorInput = color.value;
+}
+
+container.addEventListener('click',(e)=>{
+	e.target.style.backgroundColor = colorInput;
+})
+
